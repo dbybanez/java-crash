783b59fa02dfd6a2d4aece7a738c56c210242cec
dbybanez <dbybanez@gmail.com>
2020-09-24 00:17:13 +0800

    mortgage calculator

--- src/com/dbybanez/Main.java
+++ src/com/dbybanez/Main.java
@@ -1,13 +1,32 @@
 package com.dbybanez;
 
+import java.sql.SQLOutput;
+import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = { 1, 2, 3, 4, 5 };
-        System.out.println(numbers.length);
+        final byte PERCENT = 100;
+        final byte MONTHS_IN_A_YEAR = 12;
 
-        System.out.println(Arrays.toString(numbers));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mortgage Calculator");
+        System.out.print("Enter Principal: ");
+        long principal = Long.parseLong(scanner.nextLine().trim());
+
+        System.out.print("Enter Annual Interest Rate: ");
+        double annualInterestRate = Double.parseDouble(scanner.nextLine().trim()) / PERCENT / MONTHS_IN_A_YEAR;
+
+        System.out.print("Enter Period (Years): ");
+        int period = Integer.parseInt(scanner.nextLine().trim()) * MONTHS_IN_A_YEAR;
+
+        double computation = (annualInterestRate * (Math.pow(1 + annualInterestRate, period)) / ((Math.pow(1 + annualInterestRate, period)) - 1)) * principal;
+
+        NumberFormat mortgage = NumberFormat.getCurrencyInstance();
+        String result = mortgage.format(computation);
+
+        System.out.println("Your Mortgage is: " + result);
     }
 }
