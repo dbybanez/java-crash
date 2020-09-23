af1e126895ae22a14fd8b1159b4b76c607a44cfe
dbybanez <dbybanez@gmail.com>
2020-09-23 21:18:01 +0800

    Java crash course

--- src/com/dbybanez/Main.java
+++ src/com/dbybanez/Main.java
@@ -1,10 +1,13 @@
 package com.dbybanez;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello world!" + "!!";
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        System.out.println(numbers.length);
 
-        System.out.println(message.endsWith("!!"));
+        System.out.println(Arrays.toString(numbers));
     }
 }
