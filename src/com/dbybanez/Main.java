38fbcb4a44132878a470b3dd4a11a1fe4a675013
dbybanez <dbybanez@gmail.com>
2020-09-26 22:55:03 +0800

    java crash continuation

--- src/com/dbybanez/Main.java
+++ src/com/dbybanez/Main.java
@@ -8,25 +8,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte PERCENT = 100;
-        final byte MONTHS_IN_A_YEAR = 12;
+//        mortgage calculator
+//        final byte PERCENT = 100;
+//        final byte MONTHS_IN_A_YEAR = 12;
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Mortgage Calculator");
+//        System.out.print("Enter Principal: ");
+//        long principal = Long.parseLong(scanner.nextLine().trim());
+//
+//        System.out.print("Enter Annual Interest Rate: ");
+//        double annualInterestRate = Double.parseDouble(scanner.nextLine().trim()) / PERCENT / MONTHS_IN_A_YEAR;
+//
+//        System.out.print("Enter Period (Years): ");
+//        int period = Integer.parseInt(scanner.nextLine().trim()) * MONTHS_IN_A_YEAR;
+//
+//        double computation = (annualInterestRate * (Math.pow(1 + annualInterestRate, period)) / ((Math.pow(1 + annualInterestRate, period)) - 1)) * principal;
+//
+//        NumberFormat mortgage = NumberFormat.getCurrencyInstance();
+//        String result = mortgage.format(computation);
+//
+//        System.out.println("Your Mortgage is: " + result);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Mortgage Calculator");
-        System.out.print("Enter Principal: ");
-        long principal = Long.parseLong(scanner.nextLine().trim());
-
-        System.out.print("Enter Annual Interest Rate: ");
-        double annualInterestRate = Double.parseDouble(scanner.nextLine().trim()) / PERCENT / MONTHS_IN_A_YEAR;
-
-        System.out.print("Enter Period (Years): ");
-        int period = Integer.parseInt(scanner.nextLine().trim()) * MONTHS_IN_A_YEAR;
-
-        double computation = (annualInterestRate * (Math.pow(1 + annualInterestRate, period)) / ((Math.pow(1 + annualInterestRate, period)) - 1)) * principal;
-
-        NumberFormat mortgage = NumberFormat.getCurrencyInstance();
-        String result = mortgage.format(computation);
-
-        System.out.println("Your Mortgage is: " + result);
     }
 }
